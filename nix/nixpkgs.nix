83699262d7d40e7dafa315876a06f6a96913cb96
chessai <chessai1996@gmail.com>
2023-01-17 19:12:40 -0800

    nixpkgs 22.05 -> 22.11

--- nix/nixpkgs.nix
+++ nix/nixpkgs.nix
@@ -1,6 +1,9 @@
-let 
-  rev = "fc07622617a373a742ed96d4dd536849d4bc1ec6";
-in builtins.fetchTarball { 
-  url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz"; 
-  sha256 = "141ni718vq6pnpspwp0m8nsr2fpn98vls5f4m7vmgk55xwfjr4bw"; 
+let
+  owner = "NixOS";
+  repo = "nixpkgs";
+  rev = "4d2b37a84fad1091b9de401eb450aae66f1a741e";
+  sha256 = "11w3wn2yjhaa5pv20gbfbirvjq6i3m7pqrq2msf0g7cv44vijwgw";
+in builtins.fetchTarball {
+  url = "https://github.com/${owner}/${repo}/archive/${rev}.tar.gz";
+  inherit sha256;
 }
