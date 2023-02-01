a2332c81aaf4eb5e2b6679cee4f441b7369f2de4
chessai <chessai1996@gmail.com>
2023-01-31 21:28:38 -0800

    wasm-server-runner: use fork that supports gzip

--- nix/wasm-server-runner/default.nix
+++ nix/wasm-server-runner/default.nix
@@ -4,18 +4,19 @@ rustPlatform.buildRustPackage rec {
   pname = "wasm-server-runner";
   version = "0.4.0";
 
+  # Fork that solves https://github.com/jakobhellermann/wasm-server-runner/issues/28
   src = fetchFromGitHub {
-    owner = "jakobhellermann";
+    owner = "heisencoder";
     repo = pname;
-    rev = "v${version}";
-    sha256 = "16shzz5fh7lhwz2mk1vrswafdvp63zkj5y9x9ni7dvpz67ndiwdv";
+    rev = "676b5eb57947ae432a9a1ea0d2d84a16c6dac909";
+    sha256 = "1xxckn4miyx2bkirfqg373k0drhflxp28vpkji8wm15w4x6w1mgl";
   };
 
   cargoPatches = [
     ./cargo-lock.patch
   ];
 
-  cargoHash = "sha256-8cB1FBDRRVEgxWzlI5tfgETTErP71yCQL/asH2/fnlE=";
+  cargoHash = "sha256-RA80pj2iU3pjIh65sw9xCzS/5JwgpbYBMmJ9HoGnkzs=";
 
   doCheck = false;
 }
