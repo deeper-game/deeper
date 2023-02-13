3a058edeff71e58fe4c8110ee72139dcc87146df
Remy Goldschmidt <taktoa@gmail.com>
2023-02-13 02:31:28 -0800

    Add quadratic and cubic bezier curves

--- src/spline.rs
+++ src/spline.rs
@@ -1,12 +1,20 @@
 use bevy::prelude::*;
 use std::ops::{Add, Sub, Mul};
 
-pub struct PiecewiseLinearSpline<S> {
-    control_points: Vec<(f32, S)>,
+pub trait Vector: Clone
+    + Copy
+    + Add<Self, Output=Self>
+    + Sub<Self, Output=Self>
+    + Mul<f32, Output=Self> {}
+impl<V: Clone + Copy + Add<V, Output=V> + Sub<V, Output=V> + Mul<f32, Output=V>>
+    Vector for V {}
+
+pub struct PiecewiseLinearSpline<V> {
+    control_points: Vec<(f32, V)>,
 }
 
-impl<S: Clone + Copy + Add<S, Output=S> + Sub<S, Output=S> + Mul<f32, Output=S>> PiecewiseLinearSpline<S> {
-    pub fn new(points: &[(f32, S)]) -> PiecewiseLinearSpline<S> {
+impl<V: Vector> PiecewiseLinearSpline<V> {
+    pub fn new(points: &[(f32, V)]) -> PiecewiseLinearSpline<V> {
         assert!(points.len() >= 2);
         assert!(points[0].0 == 0.0);
         assert!(points[points.len() - 1].0 == 1.0);
@@ -22,7 +30,7 @@ impl<S: Clone + Copy + Add<S, Output=S> + Sub<S, Output=S> + Mul<f32, Output=S>>
         }
     }
 
-    pub fn interpolate(&self, input: f32) -> S {
+    pub fn interpolate(&self, input: f32) -> V {
         for i in 1 .. self.control_points.len() {
             let input = f32::clamp(input, 0.0, 1.0);
             let (lower_x, lower_y) = self.control_points[i - 1];
@@ -37,7 +45,53 @@ impl<S: Clone + Copy + Add<S, Output=S> + Sub<S, Output=S> + Mul<f32, Output=S>>
     }
 }
 
-#[derive(Clone, Debug)]
+pub struct QuadraticBezierCurve<V> {
+    pub p0: V,
+    pub p1: V,
+    pub p2: V,
+}
+
+impl<V: Vector> QuadraticBezierCurve<V> {
+    pub fn interpolate(&self, t: f32) -> V {
+        ((self.p0 * (1.0 - t) + self.p1 * t) * (1.0 - t))
+            + ((self.p1 * (1.0 - t) + self.p2 * t) * t)
+    }
+
+    pub fn derivative(&self, t: f32) -> V {
+        unimplemented!()
+    }
+}
+
+pub struct CubicBezierCurve<V> {
+    pub p0: V,
+    pub p1: V,
+    pub p2: V,
+    pub p3: V,
+}
+
+impl<V: Vector> CubicBezierCurve<V> {
+    pub fn interpolate(&self, t: f32) -> V {
+        let a = QuadraticBezierCurve {
+            p0: self.p0,
+            p1: self.p1,
+            p2: self.p2,
+        };
+        let b = QuadraticBezierCurve {
+            p0: self.p1,
+            p1: self.p2,
+            p2: self.p3,
+        };
+        (a.interpolate(t) * (1.0 - t)) + (b.interpolate(t) * t)
+    }
+
+    pub fn derivative(&self, t: f32) -> V {
+        (self.p1 - self.p0) * (3.0 * (1.0 - t) * (1.0 - t))
+            + (self.p2 - self.p1) * (6.0 * (1.0 - t) * t)
+            + (self.p3 - self.p2) * (3.0 * t * t)
+    }
+}
+
+#[derive(Clone, Copy, Debug)]
 pub struct Frame {
     pub forward: Vec3, // t
     pub up: Vec3, // r
